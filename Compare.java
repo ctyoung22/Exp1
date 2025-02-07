15c26fb4cd265fb69ffb7703996a5248fda8842c
ctyoung22 <connertyoung22@gmail.com>
2025-02-07 16:54:32 -0600

    Add necessary methods to classes

--- Compare.java
+++ Compare.java
@@ -2,4 +2,17 @@ package Exp1;
 
 public class Compare {
     
+    public static int iterativeF(int input) {
+
+        return 0;
+    }
+
+    public static int recursiveF(int input) {
+
+        return 0;
+    }
+
+    public static void main(String[]args) {
+        
+    }
 }
