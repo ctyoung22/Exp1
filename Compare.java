569a995ddf16f4b2f08240dbf9c2816f6aa37da2
Colson-Porterfield <143518829+Colson-Porterfield@users.noreply.github.com>
2025-02-07 18:39:51 -0600

    Update Compare.java
    Added iterativeF() functionality

--- Compare.java
+++ Compare.java
@@ -2,9 +2,29 @@ package Exp1;
 
 public class Compare {
     
-    public static int iterativeF(int input) {
+    public static int iterativeF(int n){
+        if(n == 0){
+            return 0;
+        }
+        else if(n == 1){
+            return 2;
+        }
+        if(n == 2){
+            return 4;
+        }
 
-        return 0;
+        int result = 0;
+        int last  = 0;
+        int second = 2;
+        int first = 4;
+
+        for(int i = 3; i <=n; i++){
+            result = first + (i * second) - (2 * last) + 5;
+            last = second;
+            second = first;
+            first = result;
+        }
+        return result; 
     }
 
     public static int recursiveF(int input) {
