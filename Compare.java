f5ed3ed6ef897d68642d8ed1c506b8a3b1539247
Colson-Porterfield <143518829+Colson-Porterfield@users.noreply.github.com>
2025-02-08 22:41:08 -0600

    Update Compare.java
    Added Iterative comments

--- Compare.java
+++ Compare.java
@@ -2,30 +2,36 @@ package Exp1;
 
 public class Compare {
     
-    public static int iterativeF(int n){
-        if(n == 0){
-            return 0;
-        }
-        else if(n == 1){
-            return 2;
-        }
-        if(n == 2){
-            return 4;
-        }
+    public static int iterativeF(int n){ 
 
-        int result = 0;
-        int last  = 0;
-        int second = 2;
-        int first = 4;
-
-        for(int i = 3; i <=n; i++){
-            result = first + (i * second) - (2 * last) + 5;
-            last = second;
-            second = first;
-            first = result;
-        }
-        return result; 
-    }
+// Base Cases 
+    if(n == 0){ 
+        return 0; 
+    } 
+    else if(n == 1){ 
+        return 2; 
+    } 
+    if(n == 2){ 
+        return 4; 
+    } 
+//Initialize loop values 
+    int result = 0; 
+    int last  = 0; 
+    int second = 2; 
+    int first = 4; 
+ 
+// Iterative loop from 3 to n 
+// Using the values from the three most recent  
+// results, compute the next term, up to the  
+// term requested in the method call. 
+    for(int i = 3; i <=n; i++){ 
+        result = first + (i * second) - (2 * last) + 5; 
+        last = second; 
+        second = first; 
+        first = result; 
+    } 
+    return result;  
+}
 
     public static int recursiveF(int n) {
       //base case of n = 0, 1, and 2 returns 0, 2, and 4 respectively
